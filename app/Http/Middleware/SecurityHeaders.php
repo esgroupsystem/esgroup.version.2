4cc69b141e72e3991a870b0b569e6c2e206c8147
ES Group Software Engineer <es.itdepartment.main@gmail.com>
2026-04-28 15:48:36 +0800

    Update SecurityHeaders.php

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -4,39 +4,51 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
 
 class SecurityHeaders
 {
     public function handle(Request $request, Closure $next)
     {
         $nonce = base64_encode(random_bytes(16));
-
         app()->instance('csp_nonce', $nonce);
-        View::share('cspNonce', $nonce);
 
         $response = $next($request);
 
         $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
-        $response->headers->set('Permissions-Policy', 'geolocation=(), microphone=(), camera=(), xr-spatial-tracking=()');
+        $response->headers->set('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
 
         if ($request->isSecure()) {
             $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
         }
 
-        $csp = implode(' ', [
-            "default-src 'self';",
-            "base-uri 'self';",
-            "form-action 'self';",
-            "img-src 'self' data: blob: https:;",
-            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
-            "font-src 'self' data: https://fonts.gstatic.com;",
-            "script-src 'self' 'nonce-{$nonce}' https://challenges.cloudflare.com https://static.cloudflareinsights.com;",
-            "frame-src https://challenges.cloudflare.com;",
-            "connect-src 'self' https://challenges.cloudflare.com https://static.cloudflareinsights.com;",
-        ]);
+        $isAuthPage = $request->is('login') || $request->is('lockscreen');
+
+        if ($isAuthPage) {
+            $csp = implode(' ', [
+                "default-src 'self';",
+                "base-uri 'self';",
+                "form-action 'self';",
+                "img-src 'self' data: blob: https:;",
+                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
+                "font-src 'self' data: https://fonts.gstatic.com;",
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://static.cloudflareinsights.com;",
+                'frame-src https://challenges.cloudflare.com;',
+                "connect-src 'self' https://challenges.cloudflare.com https://static.cloudflareinsights.com;",
+            ]);
+        } else {
+            $csp = implode(' ', [
+                "default-src 'self';",
+                "base-uri 'self';",
+                "form-action 'self';",
+                "img-src 'self' data: blob: https:;",
+                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
+                "font-src 'self' data: https://fonts.gstatic.com;",
+                "script-src 'self' 'nonce-{$nonce}' https://static.cloudflareinsights.com;",
+                "connect-src 'self' https://static.cloudflareinsights.com;",
+            ]);
+        }
 
         $response->headers->remove('Require-Trusted-Types-For');
         $response->headers->remove('Trusted-Types');
